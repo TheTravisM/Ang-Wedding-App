8a5a2837b4c854aa4899e5168658727037d01bf6
Travis E Mikolay <travisemikolay@gmail.com>
2018-01-13 12:22:00 -0500

    Add wedding party component

---
+++ src/app/wedding-party/wedding-party.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wedding-party',
+  templateUrl: './wedding-party.component.html',
+  styleUrls: ['./wedding-party.component.scss']
+})
+export class WeddingPartyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
