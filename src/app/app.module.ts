fa0a656e573f38c746094d461985313e0837e138
Travis E Mikolay <travisemikolay@gmail.com>
2018-01-13 12:22:16 -0500

    Add Update App Moduel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WeddingPartyComponent } from './wedding-party/wedding-party.component';
+import { HeaderComponent } from './header/header.component';
+import { CountdownComponent } from './countdown/countdown.component';
+import { LocationComponent } from './location/location.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { RegistryComponent } from './registry/registry.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeddingPartyComponent,
+    HeaderComponent,
+    CountdownComponent,
+    LocationComponent,
+    OurStoryComponent,
+    RegistryComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
