f7b5cb61f1d3e57b7b3de2cb012f7f5fea0983a7
Travis Mikolay <tmikolay@msas-MacBook-Pro-4.local>
2018-03-08 19:35:27 -0500

    add Updates

--- src/app/03-wedding-party/wedding-party-data.ts
+++ src/app/03-wedding-party/wedding-party-data.ts
@@ -2,69 +2,69 @@ export const Party = {
    'bridesmaids': [
       {
          'name' : 'Megan Barker',
-         'image' : './assets/img/wedding-party/ashden250.jpg'
+         'image' : './assets/img/wedding-party/megan-copy.jpg'
       },
       {
          'name' : 'Kim Klump',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/kim-250.jpg'
       },
       {
          'name' : 'Ashden Stark',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/ashden-250.jpg'
       },
       {
-         'name' : 'Jeanette',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'name' : 'Jeanette Webb',
+         'image' : './assets/img/wedding-party/jeanette-250.jpg'
       },
       {
          'name' : 'Brittany Beard',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/brittany-250.jpg'
       },
       {
          'name' : 'Jackie Tannreuther',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/jackie-250.jpg'
       },
       {
-         'name' : 'girl1',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'name' : 'Hailey Barker',
+         'image' : './assets/img/wedding-party/hailey-250.jpg'
       },
       {
-         'name' : 'girl1',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'name' : 'Grace Barker',
+         'image' : './assets/img/wedding-party/grace-250.jpg'
       }
    ],
    'groomsmen': [
       {
          'name' : 'Bryan',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/bryan-250.jpg'
       },
       {
          'name' : 'Shawn',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/shawn-250.jpg'
       },
       {
          'name' : 'Jerry',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/jerry-250.jpg'
       },
       {
          'name' : 'Tom',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/tommy-250.jpg'
       },
       {
          'name' : 'James',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/james-250.jpg'
       },
       {
          'name' : 'John',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/john-250.jpg'
       },
       {
          'name' : 'Joe',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/joe-250.jpg'
       },
       {
          'name' : 'Elliot',
-         'image' : './assets/img/wedding-party/ashden.jpg'
+         'image' : './assets/img/wedding-party/eliot-250.jpg'
       }
    ]
 };
