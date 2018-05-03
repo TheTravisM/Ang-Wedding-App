68b8a9511d954fbbf2cb950c51931997c8e583b3
Travis Mikolay <tmikolay@msas-MacBook-Pro-4.local>
2018-05-03 10:19:38 -0400

    Add Updates to index.

--- src/app/03-wedding-party/wedding-party-data.ts
+++ src/app/03-wedding-party/wedding-party-data.ts
@@ -2,69 +2,85 @@ export const Party = {
    'bridesmaids': [
       {
          'name' : 'Megan Barker',
-         'image' : './assets/img/wedding-party/megan-250.jpg'
+         'image' : './assets/img/wedding-party/megan-250.jpg',
+         'webp' : './assets/img/wedding-party/megan-250.webp'
       },
       {
          'name' : 'Kim Klump',
-         'image' : './assets/img/wedding-party/kim-250.jpg'
+         'image' : './assets/img/wedding-party/kim-250.jpg',
+         'webp' : './assets/img/wedding-party/kim-250.webp'
       },
       {
          'name' : 'Ashden Stark',
-         'image' : './assets/img/wedding-party/ashden-250.jpg'
+         'image' : './assets/img/wedding-party/ashden-250.jpg',
+         'webp' : './assets/img/wedding-party/ashden-250.webp'
       },
       {
          'name' : 'Jeanette Webb',
-         'image' : './assets/img/wedding-party/jeanette-250.jpg'
+         'image' : './assets/img/wedding-party/jeanette-250.jpg',
+         'webp' : './assets/img/wedding-party/jeanette-250.webp'
       },
       {
          'name' : 'Brittany Beard',
-         'image' : './assets/img/wedding-party/brittany-250.jpg'
+         'image' : './assets/img/wedding-party/brittany-250.jpg',
+         'webp' : './assets/img/wedding-party/brittany-250.webp'
       },
       {
          'name' : 'Jackie Tannreuther',
-         'image' : './assets/img/wedding-party/jackie-250.jpg'
+         'image' : './assets/img/wedding-party/jackie-250.jpg',
+         'webp' : './assets/img/wedding-party/jackie-250.webp'
       },
       {
          'name' : 'Hailey Barker',
-         'image' : './assets/img/wedding-party/hailey-250.jpg'
+         'image' : './assets/img/wedding-party/hailey-250.jpg',
+         'webp' : './assets/img/wedding-party/hailey-250.webp'
       },
       {
          'name' : 'Grace Barker',
-         'image' : './assets/img/wedding-party/grace-250.jpg'
+         'image' : './assets/img/wedding-party/grace-250.jpg',
+         'webp' : './assets/img/wedding-party/grace-250.webp'
       }
    ],
    'groomsmen': [
       {
          'name' : 'Bryan Mikolay',
-         'image' : './assets/img/wedding-party/bryan-250.jpg'
+         'image' : './assets/img/wedding-party/bryan-250.jpg',
+         'webp' : './assets/img/wedding-party/bryan-250.webp'
       },
       {
         'name' : 'James Mikolay',
-        'image' : './assets/img/wedding-party/james-250.jpg'
+        'image' : './assets/img/wedding-party/james-250.jpg',
+        'webp' : './assets/img/wedding-party/james-250.webp'
      },
      {
         'name' : 'John Mikolay',
-        'image' : './assets/img/wedding-party/john-250.jpg'
+        'image' : './assets/img/wedding-party/john-250.jpg',
+        'webp' : './assets/img/wedding-party/john-250.webp'
      },
      {
         'name' : 'Joe Mikolay',
-        'image' : './assets/img/wedding-party/joe-250.jpg'
+        'image' : './assets/img/wedding-party/joe-250.jpg',
+        'webp' : './assets/img/wedding-party/joe-250.webp'
      },
      {
         'name' : 'Eliot Pacella',
-        'image' : './assets/img/wedding-party/eliot-250.jpg'
+        'image' : './assets/img/wedding-party/eliot-250.jpg',
+        'webp' : './assets/img/wedding-party/eliot-250.webp'
      },
      {
          'name' : 'Shawn Barker',
-         'image' : './assets/img/wedding-party/shawn-250.jpg'
+         'image' : './assets/img/wedding-party/shawn-250.jpg',
+         'webp' : './assets/img/wedding-party/shawn-250.webp'
       },
       {
          'name' : 'Jerry Barker',
-         'image' : './assets/img/wedding-party/jerry-250.jpg'
+         'image' : './assets/img/wedding-party/jerry-250.jpg',
+         'webp' : './assets/img/wedding-party/jerry-250.webp'
       },
       {
          'name' : 'Tom Barker',
-         'image' : './assets/img/wedding-party/tommy-250.jpg'
+         'image' : './assets/img/wedding-party/tommy-250.jpg',
+         'webp' : './assets/img/wedding-party/tommy-250.webp'
       }
    ]
 };
